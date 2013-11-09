b4c9a9fd8fb69c9d185e62fdd767d12d0fe6f852
Vincent Huang <vincent@Vincents-MacBook-Air.local>
2013-11-09 22:44:01 +0800

    Add site status and ping function

--- setup.sh
+++ setup.sh
@@ -13,8 +13,7 @@ FILES="cn regkey.py pexpect.py"
 mkdir -p $DEPLOY_FOLDER
 su $SUDO_USER -c "touch $HOME/conn.data"
 
-for file in $FILES;
-do
+for file in $FILES;do
 	cp $file $DEPLOY_FOLDER
 	chmod +x $DEPLOY_FOLDER/$file
 done
