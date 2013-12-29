760fdaa48a77678c37a4f1412cece8e8b01b2cd1
Vincent Huang <vincent@Vincents-MacBook-Air.local>
2013-12-29 15:04:20 +0800

    Fix a bug of setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 
-function upgrade_data()
-{
-}
-
 # Check the deploy OS is Linux or Mac(Darwin)
 os=`uname`
 
@@ -13,6 +9,5 @@ else
 fi
 
 mkdir -p $DEPLOY_FOLDER
-upgrade_data
 su $SUDO_USER -c "touch $HOME/conn.data"
 cp cn.sh $DEPLOY_FOLDER/cn
